ff654c5881278e9d62af1e749f3ea3820b159bd3
Dave Longley <dlongley@digitalbazaar.com>
2018-09-19 12:31:48 -0400

    Use bedrock-web-mock-private-remote-storage.

--- test/web/mock.js
+++ test/web/mock.js
@@ -3,6 +3,7 @@
  */
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+import {MockStorage} from 'bedrock-web-mock-private-remote-storage';
 
 export const mock = {};
 
@@ -18,170 +19,10 @@ const staticMasterKey = {
 };
 
 mock.init = () => {
-  const mock = new MockAdapter(axios);
+  const mockAdapter = new MockAdapter(axios);
 
   const storages = {};
 
-  storages.test = new MockStorage({accountId: 'test', mock});
+  storages.test = new MockStorage({accountId: 'test', mockAdapter});
   storages.test.masterKey = staticMasterKey;
 };
-
-class MockStorage {
-  constructor({accountId, mock}) {
-    this.accountId = accountId;
-    this.masterKey = null;
-    this.documents = new Map();
-    this.indexes = {
-      equals: new Map(),
-      has: new Map()
-    };
-
-    const root = `/private-storage/${accountId}`;
-    const routes = {
-      documents: `${root}/documents`,
-      masterKey: `${root}/master-key`,
-      query: `${root}/query`
-    };
-
-    mock.onPut(routes.masterKey).reply(config => {
-      // TODO: check headers for `If-None-Match: *`
-      // TODO: support replacing master key?
-      if(this.masterKey) {
-        return [304];
-      }
-      this.masterKey = JSON.parse(config.data);
-      return [204];
-    });
-
-    mock.onGet(routes.masterKey).reply(() => {
-      if(!this.masterKey) {
-        return [404];
-      }
-      return [200, JSON.stringify(this.masterKey)];
-    });
-
-    mock.onPost(routes.documents).reply(config => {
-      const encryptedDoc = JSON.parse(config.data);
-      if(this.documents.has(encryptedDoc.id)) {
-        return [409];
-      }
-      this.store(encryptedDoc);
-      const location =
-        `http://localhost:9876/${routes.documents}/${encryptedDoc.id}`;
-      return [201, undefined, {location}];
-    });
-
-    const docIdRoute = new RegExp(
-      `/private-storage/${accountId}/documents/([-_A-Za-z0-9]+)`);
-
-    mock.onPut(docIdRoute).reply(config => {
-      const encryptedDoc = JSON.parse(config.data);
-      const [, id] = config.url.match(docIdRoute);
-      if(id !== encryptedDoc.id) {
-        return [400];
-      }
-      this.store(encryptedDoc);
-      return [200];
-    });
-
-    mock.onGet(docIdRoute).reply(config => {
-      const [, id] = config.url.match(docIdRoute);
-      if(!this.documents.has(id)) {
-        return [404];
-      }
-      return [200, JSON.stringify(this.documents.get(id))];
-    });
-
-    mock.onDelete(docIdRoute).reply(config => {
-      const [, id] = config.url.match(docIdRoute);
-      if(!this.documents.has(id)) {
-        return [404];
-      }
-      this.documents.delete(id);
-      return [204];
-    });
-
-    mock.onPost(routes.query).reply(config => {
-      const query = JSON.parse(config.data);
-      const results = [];
-      if(query.equals) {
-        for(const equals of query.equals) {
-          let matches = null;
-          for(const key in equals) {
-            const value = equals[key];
-            const docs = this.find(
-              {index: this.indexes.equals, key: key + '=' + value});
-            if(!matches) {
-              // first result
-              matches = docs;
-            } else {
-              // remove any docs from `matches` that are not in `docs`
-              matches = matches.filter(x => docs.includes(x));
-              if(matches.length === 0) {
-                break;
-              }
-            }
-          }
-          (matches || []).forEach(x => {
-            if(!results.includes(x)) {
-              results.push(x);
-            }
-          });
-        }
-      }
-
-      if(query.has) {
-        let matches = null;
-        for(const key of query.has) {
-          const docs = this.find({index: this.indexes.has, key});
-          if(!matches) {
-            // first result
-            matches = docs;
-          } else {
-            // remove any docs from `matches` that are not in `docs`
-            matches = matches.filter(x => docs.includes(x));
-            if(matches.length === 0) {
-              break;
-            }
-          }
-        }
-        results.push(...(matches || []));
-      }
-
-      return [200, results];
-    });
-  }
-
-  store(encryptedDoc) {
-    this.documents.set(encryptedDoc.id, encryptedDoc);
-    for(const attribute of encryptedDoc.attributes) {
-      this.addToIndex({
-        index: this.indexes.equals,
-        key: attribute.name + '=' + attribute.value,
-        encryptedDoc
-      });
-      this.addToIndex({
-        index: this.indexes.has,
-        key: attribute.name,
-        encryptedDoc
-      });
-    }
-  }
-
-  addToIndex({index, key, encryptedDoc}) {
-    let docSet = index.get(key);
-    if(!docSet) {
-      docSet = new Set();
-      index.set(key, docSet);
-    }
-    docSet.add(encryptedDoc);
-  }
-
-  find({index, key}) {
-    const docSet = index.get(key);
-    if(!docSet) {
-      return [];
-    }
-    return [...docSet];
-  }
-}
