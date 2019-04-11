3245106b97d91cb9ff8ddc492afbab212cc7ad30
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-11 12:28:20 -0400

    Add comment.

--- test/web/10-api.js
+++ test/web/10-api.js
@@ -15,7 +15,10 @@ describe('describe', () => {
   before(async () => {
     const kmsPlugin = 'ssm-v1';
     const kmsService = new KmsService();
+
+    // this corresponds to the account setup in setup-accounts.js
     const accountId = 'urn:uuid:e534fa02-b136-4ff1-943d-4f88458f6324';
+
     kmsApi = await AccountMasterKey.fromSecret({
       accountId,
       kmsPlugin,
